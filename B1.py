0392d1f3586653151d68cc1937537cabc1871b48
DannBreel <danielbkr.gabriel@gmail.com>
2026-03-21 14:26:26 -0300

    Add files via upload

---
+++ B1.py
@@ -0,0 +1,62 @@
+nome=input("Seu nome é?")
+print("bem vindo",nome,"!")
+
+######################
+print(" ")
+
+at2 = int(input("numero pls:"))
+print(at2,at2+1,at2-1)
+
+######################
+print(" ")
+
+print(float(at2+0.12))
+
+######################
+print(" ")
+
+atv4=int(input("idade?"))
+print("você tem",atv4,"anos!")
+
+######################
+print(" ")
+
+atv5 = input("altura pls:")
+Aa5 = [nome,atv4,atv5]
+
+print(" ")
+
+for i in Aa5:
+    print(i)
+
+######################
+print(" ")
+
+print("seu nome tem",len(nome),"letras!")
+
+######################
+print(" ")
+print(nome.lower())
+print(nome.upper())
+
+######################
+print(" ")
+
+print(atv4>18)
+
+######################
+print(" ")
+
+lo1="aiai meu bumbumn"
+lo2="nada ve zé"
+print(lo1)
+print(lo2)
+print(" ")
+print(lo1,lo2)
+
+######################
+print(" ")
+city = str(input("city é?"))
+plp = int(input("habitants?"))
+
+print("cidade",city,",número de habitantes =",plp)
