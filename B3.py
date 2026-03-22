76d15a57937f8d63b147de6c7b942304e85236d5
DannBreel <danielbkr.gabriel@gmail.com>
2026-03-21 21:27:08 -0300

    Add files via upload

---
+++ B3.py
@@ -0,0 +1,76 @@
+print("\033[43;30m     Atividade 21    \033[0m")
+print(" ")
+
+print("digite 2 numbers")
+a1=int(input())
+a2=int(input())
+
+print("igual??",a1==a2,"|diferentee???",a1!=a2)
+
+print(" ")
+print("\033[43;30m     Atividade 22    \033[0m")
+print(" ")
+
+ida=int(input("idade??"))
+print("maioridade =",ida>17)
+
+print(" ")
+print("\033[43;30m     Atividade 23    \033[0m")
+print(" ")
+
+print(False,True)
+print(False and True)
+print(False or True)
+print(not False,not True)
+
+print(" ")
+print("\033[43;30m     Atividade 24    \033[0m")
+print(" ")
+
+print(ida)
+print("é ]10,20[??",10<ida<20)
+
+print(" ")
+print("\033[43;30m     Atividade 25    \033[0m")
+print(" ")
+
+nota = int(input("nota?"))
+print("entre 0 e 10:",0>nota>10)
+
+print(" ")
+print("\033[43;30m     Atividade 26    \033[0m")
+print(" ")
+
+print(a1,a2)
+ab1=a1>0 and a2>0
+print("ambos positivos?",ab1)
+
+print(" ")
+print("\033[43;30m     Atividade 27    \033[0m")
+print(" ")
+
+print(a1,a2)
+ab2=a1%2 ==0 and a2%1 ==0
+print("positivos e pares??",ab1 and ab2)
+
+print(" ")
+print("\033[43;30m     Atividade 28    \033[0m")
+print(" ")
+
+print(a1,a2,ida)
+print("primeiro é maior que o segundo e menor que o terceiro?",a1>a2 and a1<ida)
+
+print(" ")
+print("\033[43;30m     Atividade 29    \033[0m")
+print(" ")
+
+use = input("nome?")
+sen = input("senha?")
+print("ambos são diferentes de string vazia??",use != " " and sen != " ")
+
+print(" ")
+print("\033[43;30m     Atividade 30    \033[0m")
+print(" ")
+
+an = int(input("ano pls:"))
+print("intervalo válido",1900<an<2100)
